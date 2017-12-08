590495f5c89f879e6c15f6c69bf136ffb9ccd872
Mateusz <mateuszhotowy@gmail.com>
2017-12-08 09:14:21 +0100

    Done.

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1,4,8,11,99};
+        int sum = numbers[0] + numbers[2] + numbers[4];
+
+        System.out.println(sum);
+    }
+}
